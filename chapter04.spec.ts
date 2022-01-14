85ead981b08b78a3705950a9525248e522bb6300
Mike Harris <MikeMKH@users.noreply.github.com>
2022-01-14 06:37:46 -0600

    add examples from chapter 4.2

--- chapter04.spec.ts
+++ chapter04.spec.ts
@@ -3,6 +3,7 @@ import { beforeEach } from 'mocha';
 
 declare const LbfsType: unique symbol;
 declare const NsType: unique symbol;
+declare const PercentageType: unique symbol;
 
 describe('chapter 4', () => {
   describe('primitive obsession', () => {
@@ -66,6 +67,70 @@ describe('chapter 4', () => {
         
         provideMomentum();
         expect(wasDisintegrated).to.be.false;
+      });
+    });
+  }),
+  describe('enforcing constraints', () => {
+    describe('Percentage', () => {
+      describe('constructor', () => {
+        // declare const PercentageType: unique symbol; // at top of file
+        class Percentage {
+          readonly value: number;
+          [PercentageType]: void;
+          
+          constructor(value: number) {
+            if (value < 0 || value > 100) {
+              throw new Error("Percentage must be between 0 and 100");
+            }
+            
+            this.value = value;
+          }
+        }
+        
+        it('should throw an Error if value < 0', () => {
+          expect(() => new Percentage(-1)).to.throw(Error);
+        }),
+        it('should throw an Error if value > 100', () => {
+          expect(() => new Percentage(101)).to.throw(Error);
+        }),
+        it('should construct type if value >= 0 and <= 100', () => {
+          expect(new Percentage(0)).to.be.ok;
+          expect(new Percentage(1)).to.be.ok;
+          expect(new Percentage(42)).to.be.ok;
+          expect(new Percentage(99)).to.be.ok;
+          expect(new Percentage(100)).to.be.ok;
+        });
+      }),
+      describe('factory', () => {
+        // declare const PercentageType: unique symbol; // at top of file
+        class Percentage {
+          readonly value: number;
+          [PercentageType]: void;
+          
+          private constructor(value: number) {
+            this.value = value;
+          }
+          static from(value: number): Percentage {
+            if (value < 0 || value > 100) {
+              throw new Error("Percentage must be between 0 and 100");
+            }
+            return new Percentage(value);
+          }
+        }
+        
+        it('should throw an Error if value < 0', () => {
+          expect(() => Percentage.from(-1)).to.throw(Error);
+        }),
+        it('should throw an Error if value > 100', () => {
+          expect(() => Percentage.from(101)).to.throw(Error);
+        }),
+        it('should construct type if value >= 0 and <= 100', () => {
+          expect(Percentage.from(0)).to.be.ok;
+          expect(Percentage.from(1)).to.be.ok;
+          expect(Percentage.from(42)).to.be.ok;
+          expect(Percentage.from(99)).to.be.ok;
+          expect(Percentage.from(100)).to.be.ok;
+        });
       });
     });
   })
