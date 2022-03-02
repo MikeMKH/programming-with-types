fb0b80a68feb08f59d3e7d880b9e11a4c4f78f43
Mike Harris <MikeMKH@users.noreply.github.com>
2022-03-02 06:38:19 -0600

    add examples from chapter 8.2

--- chapter08.spec.ts
+++ chapter08.spec.ts
@@ -88,5 +88,101 @@ describe('object oriented programming', () => {
       let b: IB = new X();
       expect(b.b()).to.equal('x'.charCodeAt(0));
     });
+  }),
+  describe('inheritance', () => {
+    interface IExpression {
+      evaluate(): number;
+    }
+    
+    abstract class BinaryExpression implements IExpression {
+      constructor(protected left: IExpression, protected right: IExpression) {
+      }
+      
+      abstract evaluate(): number;
+    }
+    
+    class Constant implements IExpression {
+      constructor(protected value: number) {
+      }
+      
+      evaluate(): number {
+        return this.value;
+      }
+    }
+    
+    class Addition extends BinaryExpression {
+      evaluate(): number {
+        return this.left.evaluate() + this.right.evaluate();
+      }
+    }
+    
+    class Multiple extends BinaryExpression {
+      evaluate(): number {
+        return this.left.evaluate() * this.right.evaluate();
+      }
+    }
+    
+    abstract class UnaryExpression implements IExpression {
+      constructor(protected expression: IExpression) {
+      }
+      
+      abstract evaluate(): number;
+    }
+    
+    class Negate extends UnaryExpression {
+      evaluate(): number {
+        return -this.expression.evaluate();
+      }
+    }
+    
+    class Subtraction implements IExpression {
+      private operation: Addition;
+      
+      constructor(left: IExpression, right: IExpression) {
+        this.operation = new Addition(left, new Negate(right));
+      }
+      
+      evaluate(): number {
+        return this.operation.evaluate();
+      }
+    }
+    
+    it('2 + 3 = 5', () => {
+      let two: IExpression = new Constant(2);
+      let three: IExpression = new Constant(3);
+      let result: IExpression = new Addition(two, three);
+      
+      expect(result.evaluate()).to.equal(5);
+    }),
+    it('2 * 3 = 6', () => {
+      let two: IExpression = new Constant(2);
+      let three: IExpression = new Constant(3);
+      let result: IExpression = new Multiple(two, three);
+      
+      expect(result.evaluate()).to.equal(6);
+    }),
+    it('2 + 3 * 4 = 14', () => {
+      let two: IExpression = new Constant(2);
+      let three: IExpression = new Constant(3);
+      let four: IExpression = new Constant(4);
+      let result: IExpression = new Addition(two, new Multiple(three, four));
+      
+      expect(result.evaluate()).to.equal(14);
+    }),
+    it('-2 + 3 * 4 = 13', () => {
+      let two: IExpression = new Constant(2);
+      let three: IExpression = new Constant(3);
+      let four: IExpression = new Constant(4);
+      let result: IExpression = new Addition(new Negate(two), new Multiple(three, four));
+      
+      expect(result.evaluate()).to.equal(10);
+    }),
+    it('3 - 2 = 1', () => {
+      let three: IExpression = new Constant(3);
+      let two: IExpression = new Constant(2);
+      let result: IExpression = new Subtraction(three, two);
+      
+      expect(result.evaluate()).to.equal(1);
+    })
   })
 })
