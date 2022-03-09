9d4813d8db1c2b627c412bcc7c78ec0ff27fbbc7
Mike Harris <MikeMKH@users.noreply.github.com>
2022-03-09 06:16:53 -0600

    add exercises from chapter 9.2

--- chapter09.spec.ts
+++ chapter09.spec.ts
@@ -46,6 +46,146 @@ describe('generic data structures', () => {
       
       let num: Box<number> = new Box<number>(10);
       expect(unbox(num)).to.equal(10);
+    });
+  }),
+  describe('Stack', () => {
+    class Stack<T> {
+      private readonly values: T[];
+      
+      constructor() {
+        this.values = [];
+      }
+      
+      push(value: T): void {
+        this.values.push(value);
+      }
+      
+      pop(): T {
+        if (this.values.length === 0) {
+          throw new Error('Stack is empty');
+        }
+        return this.values.pop()!;
+      }
+      
+      peek(): T {
+        if (this.values.length === 0) {
+          throw new Error('Stack is empty');
+        }
+        return this.values[this.values.length - 1];
+      }
+    }
+    it('should add item with push', () => {
+      const stack = new Stack<number>();
+      stack.push(10);
+      expect(stack.peek()).to.equal(10);
+    }),
+    it('should remove item with pop', () => {
+      const stack = new Stack<number>();
+      stack.push(10);
+      expect(stack.pop()).to.equal(10);
+    }),
+    it('should throw error when popping empty stack', () => {
+      const stack = new Stack<number>();
+      expect(() => stack.pop()).to.throw(Error);
+    }),
+    it('should throw error when peeking empty stack', () => {
+      const stack = new Stack<number>();
+      expect(() => stack.peek()).to.throw(Error);
+    }),
+    it('should be able to peek at the top of the stack', () => {
+      const stack = new Stack<string>();
+      stack.push('hello');
+      expect(stack.peek()).to.equal('hello');
+    }),
+    it('should be LIFO', () => {
+      const stack = new Stack<string>();
+      stack.push('hello');
+      stack.push('world');
+      expect(stack.pop()).to.equal('world');
+      expect(stack.pop()).to.equal('hello');
+    });
+  }),
+  describe('Queue', () => {
+    class Queue<T> {
+      private readonly values: T[];
+      
+      constructor() {
+        this.values = [];
+      }
+      
+      enqueue(value: T): void {
+        this.values.push(value);
+      }
+      
+      dequeue(): T {
+        if (this.values.length === 0) {
+          throw new Error('Queue is empty');
+        }
+        return this.values.shift()!;
+      }
+      
+      peek(): T {
+        if (this.values.length === 0) {
+          throw new Error('Queue is empty');
+        }
+        return this.values[0];
+      }
+    }
+    
+    it('should add item with enqueue', () => {
+      const queue = new Queue<number>();
+      queue.enqueue(10);
+      expect(queue.peek()).to.equal(10);
+    }),
+    it('should remove item with dequeue', () => {
+      const queue = new Queue<number>();
+      queue.enqueue(10);
+      expect(queue.dequeue()).to.equal(10);
+    }),
+    it('should throw error when dequeuing empty queue', () => {
+      const queue = new Queue<number>();
+      expect(() => queue.dequeue()).to.throw(Error);
+    }),
+    it('should throw error when peeking empty queue', () => {
+      const queue = new Queue<number>();
+      expect(() => queue.peek()).to.throw(Error);
+    }),
+    it('should be able to peek at the top of the queue', () => {
+      const queue = new Queue<string>();
+      queue.enqueue('hello');
+      expect(queue.peek()).to.equal('hello');
+    }),
+    it('should be FIFO', () => {
+      const queue = new Queue<string>();
+      queue.enqueue('hello');
+      queue.enqueue('world');
+      expect(queue.dequeue()).to.equal('hello');
+      expect(queue.dequeue()).to.equal('world');
     })
+  }),
+  describe('Pair', () => {
+    class Pair<T, U> {
+      private readonly first: T;
+      private readonly second: U;
+      
+      constructor(first: T, second: U) {
+        this.first = first;
+        this.second = second;
+      }
+      
+      getFirst(): T {
+        return this.first;
+      }
+      
+      getSecond(): U {
+        return this.second;
+      }
+    }
+    
+    it('should be able to get first and second', () => {
+      const pair = new Pair<number, string>(10, 'hello');
+      expect(pair.getFirst()).to.equal(10);
+      expect(pair.getSecond()).to.equal('hello');
+    });
   })
 });
