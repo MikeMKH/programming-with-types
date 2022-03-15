2db4d24f5aa3afb3374b89d7dada9306540a7107
Mike Harris <MikeMKH@users.noreply.github.com>
2022-03-15 06:34:49 -0500

    add exercises from chapter 10.2

--- chapter10.spec.ts
+++ chapter10.spec.ts
@@ -39,6 +39,115 @@ describe('generic algorithms', () => {
         
       expect(pipeline(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
         .to.deep.equal(['FizzBuzz', '1', '2', 'Fizz', '4', 'Buzz', 'Fizz', '7', '8', 'Fizz', 'Buzz']);
-    })
+    });
+  }),
+  describe('composition', () => {
+    class FluentIterable<T> {
+      private iter: Iterable<T>;
+      constructor(iter: Iterable<T>) {
+        this.iter = iter;
+      }
+      
+      result(): Iterable<T> {
+        let result: T[] = [];
+        for (const item of this.iter) {
+          result.push(item);
+        }
+        return result;
+      }
+      
+      private *mapImpl<U>(f: (x: T) => U): IterableIterator<U> {
+        for (const x of this.iter) {
+          yield f(x);
+        }
+      }
+      
+      map<U>(f: (x: T) => U): FluentIterable<U> {
+        return new FluentIterable(this.mapImpl(f));
+      }
+      
+      private *filterImpl(f: (x: T) => boolean): IterableIterator<T> {
+        for (const x of this.iter) {
+          if (f(x)) {
+            yield x;
+          }
+        }
+      }
+      
+      filter(f: (x: T) => boolean): FluentIterable<T> {
+        return new FluentIterable(this.filterImpl(f));
+      }
+      
+      reduce<U>( initial: U, f: (acc: U, x: T) => U): U {
+        let acc = initial;
+        for (const x of this.iter) {
+          acc = f(acc, x);
+        }
+        return acc;
+      }
+      
+      private *takeImpl(n: number): IterableIterator<T> {
+        let i = 0;
+        for (const x of this.iter) {
+          if (i++ >= n) {
+            break;
+          }
+          yield x;
+        }
+      }
+      
+      take(n: number): FluentIterable<T> {
+        return new FluentIterable(this.takeImpl(n));
+      }
+      
+      private *dropImpl(n: number): IterableIterator<T> {
+        let i = 0;
+        for (const x of this.iter) {
+          if (i++ >= n) {
+            yield x;
+          }
+        }
+      }
+      
+      drop(n: number): FluentIterable<T> {
+        return new FluentIterable(this.dropImpl(n));
+      }
+    }
+    
+    it('should give results', () => {
+      const result = (new FluentIterable([1, 2, 3, 4, 5])).result();
+      expect(result).to.deep.equal([1, 2, 3, 4, 5]);
+    }),
+    it('should be able to map', () => {
+      const result = (new FluentIterable([1, 2, 3, 4, 5])).map(x => x * x).result();
+      expect(result).to.deep.equal([1, 4, 9, 16, 25]);
+    }),
+    it('should be able to filter', () => {
+      const result = (new FluentIterable([1, 2, 3, 4, 5])).filter(x => x % 2 === 0).result();
+      expect(result).to.deep.equal([2, 4]);
+    }),
+    it('should be able to reduce', () => {
+      const result = (new FluentIterable([1, 2, 3, 4, 5])).reduce(0, (acc, x) => acc + x);
+      expect(result).to.equal(15);
+    }),
+    it('should be able to compose', () => {
+      const result = (new FluentIterable([1, 2, 3, 4, 5]))
+        .map(x => x * x)
+        .filter(x => x % 2 === 0)
+        .reduce(0, (acc, x) => acc + x);
+      expect(result).to.equal(20);
+    }),
+    it('should be able to take', () => {
+      const result = (new FluentIterable([1, 2, 3, 4, 5]))
+        .take(3)
+        .result();
+      expect(result).to.deep.equal([1, 2, 3]);
+    }),
+    it('should be able to drop', () => {
+      const result = (new FluentIterable([1, 2, 3, 4, 5]))
+        .drop(3)
+        .result();
+      expect(result).to.deep.equal([4, 5]);
+    });
   })
 })
